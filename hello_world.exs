9c65456b14106b519bbe890c5c3ef41c16bf3520
kevinbreaker <kevinbreaker2604@gmail.com>
2018-06-30 19:13:33 -0300

    Hello world - Elixir lang

---
+++ hello_world.exs
@@ -0,0 +1,8 @@
+# Primeiro Hello World com Elixir =D
+## My first Hello World in Elixir lang :D
+
+IO.puts("yey, My first hello world in Elixir")
+IO.puts("Hello " <> "World =D")
+
+world = [?W, ?o, ?r, ?l, ?d]
+IO.puts([?H, ?e, ?l, ?l, ?o] ++ world)
